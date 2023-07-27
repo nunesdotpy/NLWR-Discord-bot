f27277185d6d3d7ef0a997e0c5bc250107121b3f
João Nunes <66601995+nunesdotpy@users.noreply.github.com>
2023-07-26 21:48:04 -0300

    Update responses.py

--- responses.py
+++ responses.py
@@ -2,7 +2,7 @@ def get_response(message: str) -> str:
     p_message = message.lower()
 
     if p_message == '$ajuda':
-        return 'Comandos:\n`$ips` Lista com os IPs dos nossos servidores.\n`$ajuda` Lista de comandos\n`p$[comando]` Responde o comando solicitado no chat privado'
+        return 'Comandos:\n`$ip` Lista com os IPs dos nossos servidores.\n`$ajuda` Lista de comandos\n`p$[comando]` Responde o comando solicitado no chat privado'
 
-    if p_message == '$ips':
-        return '```CSS\n[Servidores da North Lion Warriors]```\nTS3```ts3.nlwr.net```\nCS MIX```connect 35.247.192.35:27015;password nlwr```\n> https://gaming.nlwr.net/'
+    if p_message == '$ip':
+        return '```CSS\n[Servidores da North Lion Warriors]```\nTS3```ts3.nlwr.net```\nCS MIX```connect 34.151.254.204:27015;password gxngaming```\n> https://gaming.nlwr.net/'
